04eb9ac58b02ad001089c79b025e0cac72012f93
binleiden <daniel.petersson83@gmail.com>
2024-05-02 12:02:41 +0200

    formated code

--- sql_1_select.sql
+++ sql_1_select.sql
@@ -15,112 +15,166 @@
 #
 # 1: Get a query to return "Hello World", 123
 # (Hint: 1 row, 2 columns)
-SELECT "hello world" as message , 123 as id;
+SELECT 'hello world' AS message, 123 AS id;
 #
 # 2: Get everything from the city table
 # (Hint: Many many rows)
-select * from city;
+SELECT 
+    *
+FROM
+    city;
 #
 # 3: Get everything on the cities whose district is "aceh"
 # (Hint: 2 rows)
-select * 
-from city
-where District = 'Aceh'; 
+SELECT 
+    *
+FROM
+    city
+WHERE
+    District = 'Aceh'; 
 #
 # 4: Get only the name of the cities where the countrycode is "bfa"
-select *
-from city
-where CountryCode = 'bfa';
+SELECT 
+    *
+FROM
+    city
+WHERE
+    CountryCode = 'bfa';
 #
 #
 # 5: Get both the name and district of the cities where the countrycode is "tto"
-select Name, District
-from city
-where CountryCode = 'tto';
+SELECT 
+    Name, District
+FROM
+    city
+WHERE
+    CountryCode = 'tto';
 #
 #
 # 6: Get the name and district named as nm,dist from the cities where the countrycode is "arm"
-select Name as nm, District as dist
-from city
-where CountryCode = 'arm';
+SELECT 
+    Name AS nm, District AS dist
+FROM
+    city
+WHERE
+    CountryCode = 'arm';
 #
 #
 # 7: Get the cities with a name that starts with "bor"
-select *
-from city
-where Name like 'bor%';
+SELECT 
+    *
+FROM
+    city
+WHERE
+    Name LIKE 'bor%';
 #
 #
 # 8: Get the cities with a name that contains the string "orto"
-select *
-from city
-where Name like '%orto%';
+SELECT 
+    *
+FROM
+    city
+WHERE
+    Name LIKE '%orto%';
 #
 #
 # 9: Get the cities that has a population below 1000
-select *
-from city
-where Population < 1000;
+SELECT 
+    *
+FROM
+    city
+WHERE
+    Population < 1000;
 #
 # 10: Get the unique countrycodes from the cities that has a population below 1000
-select distinct CountryCode
-from city
-where Population <1000;
+SELECT DISTINCT
+    CountryCode
+FROM
+    city
+WHERE
+    Population < 1000;
 #
 # 11: Get the cities with the countrycode UKR that has more than 1000000 (one million) in population
-SELECT *
-FROM city
-WHERE CountryCode = 'UKR' AND Population > 1000000;
+SELECT 
+    *
+FROM
+    city
+WHERE
+    CountryCode = 'UKR'
+        AND Population > 1000000;
 #
 #
 # 12: Get the cities with a population of below 200 or above 9500000 (9.5 million)
-select *
-from	city
-where Population <200 or Population >9500000;
+SELECT 
+    *
+FROM
+    city
+WHERE
+    Population < 200 OR Population > 9500000;
 #
 #
 # 13: Get the cities with the countrycodes TJK, MRT, AND, PNG, SJM
-select * 
-from	city
-where	CountryCode in	('TJK', 'MRT', 'PNG', 'SJM');
+SELECT 
+    *
+FROM
+    city
+WHERE
+    CountryCode IN ('TJK' , 'MRT', 'PNG', 'SJM');
 #
 #
 # 14: Get the cities with a population between 200 and 700 inclusive
-select*
-from	city
-where Population between 200 and 700;
+SELECT 
+    *
+FROM
+    city
+WHERE
+    Population BETWEEN 200 AND 700;
 #
 #
 # 15: Get the countries with a population between 8000 and 20000 inclusive
-SELECT country.Name, SUM(city.Population) AS total_population
-FROM country
-JOIN city ON country.Code = city.CountryCode
+SELECT 
+    country.Name, SUM(city.Population) AS total_population
+FROM
+    country
+        JOIN
+    city ON country.Code = city.CountryCode
 GROUP BY country.Name
 HAVING total_population BETWEEN 8000 AND 20000;
 
 #
 #
 # 16: Get the name of the countries with a independence year (indepyear) before year 0
-SELECT Name
-FROM country
-WHERE indepyear < 0;
+SELECT 
+    Name
+FROM
+    country
+WHERE
+    indepyear < 0;
 
 #
 #
 # 17: Get the countries that has no recorded independence year and a population above 1000000
-SELECT country.Name
-FROM country
-LEFT JOIN city ON country.Code = city.CountryCode
+SELECT 
+    country.Name
+FROM
+    country
+        LEFT JOIN
+    city ON country.Code = city.CountryCode
 GROUP BY country.Name
-HAVING MAX(city.Population) > 1000000 AND COUNT(city.Population) = 0;
+HAVING MAX(city.Population) > 1000000
+    AND COUNT(city.Population) = 0;
 
 #
 #
 # 18: Get countries with a SurfaceArea below 10 and a defined LifeExpectancy
-SELECT c.Name
-FROM country c
-JOIN countrylanguage cl ON c.Code = cl.CountryCode
-WHERE c.SurfaceArea < 10
-AND c.LifeExpectancy IS NOT NULL;
+SELECT 
+    c.Name
+FROM
+    country c
+        JOIN
+    countrylanguage cl ON c.Code = cl.CountryCode
+WHERE
+    c.SurfaceArea < 10
+        AND c.LifeExpectancy IS NOT NULL;
 
 #
